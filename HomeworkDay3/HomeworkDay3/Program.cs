7873216f427280f66e239b3373dde566e715f625
Kotek506 <leonidias560@gmail.com>
2024-01-06 13:59:03 +0100

    homework day 3

---
+++ HomeworkDay3/HomeworkDay3/Program.cs
@@ -0,0 +1,16 @@
+﻿var name = "Ewa";
+var gender = "man";
+var age = 11;
+
+if(gender == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if(gender == "man" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
